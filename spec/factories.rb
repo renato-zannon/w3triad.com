6350df81db4a3fd5220f23d5259d3e4080e29e75
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-22 18:30:42 -0300

    Created Post model

---
+++ spec/factories.rb
@@ -0,0 +1,4 @@
+Factory.define :post do |f|
+  f.title "Lorem ipsum"
+  f.content  "Lorem ipsum dolor sit amet"
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Post do
+  subject { Factory.build(:post) }
+
+  it "is valid with valid attributes" do
+    subject.should be_valid
+  end
+
+  it "isn't valid without a title" do
+    ["", nil].each do |invalid_title|
+      subject.title = invalid_title
+      subject.should_not be_valid
+    end
+  end
+
+  it "isn't valid without a content" do
+    ["", nil].each do |invalid_content|
+      subject.content = invalid_content
+      subject.should_not be_valid
+    end
+  end
+
+end
