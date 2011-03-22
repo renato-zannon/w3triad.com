b86f51869bee21eae9155892bd3df5e40e87e79c
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-22 19:42:05 -0300

    Fixed a few typing errors

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -3,8 +3,8 @@ Given /^I click on the "([^"]*)" link$/ do |link_name|
 end
 
 When /^I create a new post$/ do
-  fill_in :title, :with => "Stub post :)"
-  fill_in :content, :with => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  fill_in "Title", :with => "Stub post :)"
+  fill_in "Content", :with => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   click_button :submit
 end
 
