301d35cf43a449958f19eb774728f5161e13f4ba
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-22 12:17:59 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+W3triadCom::Application.config.secret_token = '23771416778c663bcba6dc5cd7dafc323b41433f1fca16e138d3fa316032fb60e4e9c32684cc762a5af6d918664af4ff852685aaab3691439c7e9a154413827f'
