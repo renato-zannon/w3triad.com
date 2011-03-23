1a2b3e7af7d15bbb151c1e1c56366dbb5c921d9b
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-23 01:45:20 -0300

    Created the Post.paragraphs method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :presence => true
+
+  def paragraphs
+    content.split(/\n{2,}/). #Separates the text on places where there are more two or more newlines (we want to keep the single ones alone)
+      map { |p| p.gsub(/(^ +)|( +$)/,'') } #Removes trailing and leading spaces
+  end
 end
