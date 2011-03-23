677190c6b4297a260218dd1d00a8a5b0b62989cf
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-23 02:19:54 -0300

    Implemented the view's <p> mechanism using Post.paragraph

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
   validates :title, :content, :presence => true
 
   def paragraphs
-    content.split(/\n{2,}/). #Separates the text on places where there are more two or more newlines (we want to keep the single ones alone)
-      map { |p| p.gsub(/(^ +)|( +$)/,'') } #Removes trailing and leading spaces
+    content.gsub(/\r/,'') #Removing troublesome carriage returns
+      .split(/\n{2,}/) #Splits the text on places where there's two or more newlines (we want to keep the single ones alone)
+      .map { |p| p.gsub(/(^ +)|( +$)/,'') } #Removes trailing and leading spaces
   end
 end
