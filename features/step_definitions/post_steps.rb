d8ef45061eb328875dea9c91da3522288ba65190
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-28 16:44:28 -0300

    Minor refactor on step definitions

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -3,9 +3,9 @@ Given /^I click on the "([^"]*)" link$/ do |link_name|
 end
 
 When /^I create a new post$/ do
-  fill_in "Title", :with => "Stub post :)"
-  fill_in "Content", :with => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  click_button :submit
+  When "I fill in \"Title\" with \"Stub post :)\""
+  And  "I fill in \"Content\" with \"Stub content!\""
+  And  "I press \"submit\""
 end
 
 Then /^I should be able to see it on the post's index$/ do
@@ -15,6 +15,5 @@ end
 
 Then /^I should be able to see the post's complete page$/ do
   page.body.should include "Stub post :)"
-  page.body.should include "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  page.body.should include "Stub content!"
 end
-
