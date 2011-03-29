4925783766d52b8b4a89fefcccea0982efd9be60
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-29 19:16:13 -0300

    Implemented Formattable#formated_content

--- app/formattable/formattable.rb
+++ app/formattable/formattable.rb
@@ -1,12 +1,22 @@
 module Formattable
 
   def tags
-    @tags ||= {}
+    @tags ||= TagCollection.new
   end
 
   def self.included(klass)
     unless klass.new.respond_to?(:content)
       raise "#{klass.name} doesn't have a content method!"
     end
+  end
+
+  def formatted_content
+    tags.inject(content) do |text, tag|
+      key = tag[0]
+      open_tag = tag[1][0]
+      close_tag = tag[1][1]
+      text.gsub!(/(^|[^\\])(\$#{key})(.*?)\2/, "\\1#{open_tag}\\3#{close_tag}")
+      text.gsub(/(^|[^\\])\$#{key}/, "\\1#{open_tag}")
+    end.gsub(/\\\$/,"$")
   end
 end
--- spec/formattable/formattable_spec.rb
+++ spec/formattable/formattable_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
-require 'formattable'
 
 class TestClass
-  def content
-    "content"
-  end
+  attr_accessor :content
 end
 
 describe Formattable do
@@ -13,6 +10,39 @@ describe Formattable do
 
   it "has a default set of tags" do
     should respond_to(:tags)
+    subject.tags.should_not be_nil
+  end
+
+
+  describe "#formatted_content" do
+    it "is defined" do
+      subject.should respond_to(:formatted_content)
+    end
+
+    it "formats simple tags" do
+      subject.content = "$tcontent$t"
+      subject.tags['t'] = ['<tag>', '</tag>']
+      subject.formatted_content.should == "<tag>content</tag>"
+    end
+
+    it "a tag doesn't interfere on another" do
+      subject.content = "$btext1$b $itext2$i $b$itext3$i$b"
+      subject.tags['b'] = ["<bold>", "</bold>"]
+      subject.tags['i'] = ["<italic>", "</italic>"]
+      subject.formatted_content.should == "<bold>text1</bold> <italic>text2</italic> <bold><italic>text3</italic></bold>"
+    end
+
+    it "leaves the tag unopened if doesn't encounter a pair of keys" do
+      subject.content = "$ttext"
+      subject.tags['t'] = ["<tag>", "</tag>"]
+      subject.formatted_content.should == "<tag>text"
+    end
+
+    it "substitutes \$ with $, and don't resolve as a tag" do
+      subject.content = '\$t$ttext$t'
+      subject.tags['t'] = ["<tag>", "</tag>"]
+      subject.formatted_content.should == "$t<tag>text</tag>"
+    end
   end
 
   it "complains if the class doesn't have a content" do
