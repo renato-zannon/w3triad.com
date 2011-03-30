43a0a28b6e9a39ed7f226a71ae376440f7205220
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-30 15:04:32 -0300

    Made the needed adjustments on formattable, to support complex tags

--- lib/formattable/formattable.rb
+++ lib/formattable/formattable.rb
@@ -27,12 +27,28 @@ module Formattable
 
   private
   def format(string)
-    tags.inject(string) do |text, tag|
+    simple_tags = tags.reject { |tag, _| tag=='()' }
+    string = simple_format(string, simple_tags)
+    complex_format(string)
+  end
+
+  def simple_format(string, simple_tags)
+    simple_tags.inject(string) do |text, tag|
       key = tag[0]
       open_tag = tag[1][0]
       close_tag = tag[1][1]
       text.gsub!(/(^|[^\\])(\$#{key})(.*?)\2/, "\\1#{open_tag}\\3#{close_tag}")
       text.gsub(/(^|[^\\])\$#{key}/, "\\1#{open_tag}")
     end.gsub(/\\\$/,"$")
+  end
+
+  def complex_format(string)
+    result = string.dup
+    string.scan(/(^|[^\\])\$(\([\w\s]+\))(.*?)\$\2/) do |starter, key, text|
+      open_tag = tags[key][0]
+      close_tag = tags[key][1]
+      result.gsub!(starter+'$'+key+text+'$'+key, starter+open_tag+text+close_tag)
+    end
+    result
   end
 end
--- spec/formattable/formattable_spec.rb
+++ spec/formattable/formattable_spec.rb
@@ -26,6 +26,12 @@ describe Formattable do
       subject.formatted_content.should == "<tag>content</tag>"
     end
 
+    it "formats complex tags" do
+      subject.content = "$(style)test$(style)"
+      subject.tags['()'] = lambda { |t| ["<#{t}>", "</#{t}>"] }
+      subject.formatted_content.should == "<style>test</style>"
+    end
+
     it "a tag doesn't interfere on another" do
       subject.content = "$btext1$b $itext2$i $b$itext3$i$b"
       subject.tags['b'] = ["<bold>", "</bold>"]
@@ -33,6 +39,12 @@ describe Formattable do
       subject.formatted_content.should == "<bold>text1</bold> <italic>text2</italic> <bold><italic>text3</italic></bold>"
     end
 
+    it "formats complex tags and simple tags altogether" do
+      subject.content = "$(style)$ttext$t$(style) $t$(style)text$(style)$t"
+      subject.tags['()'] = lambda { |t| ["<#{t}>", "</#{t}>"] }
+      subject.tags['t'] = ['<test>', '</test>']
+      subject.formatted_content.should == "<style><test>text</test></style> <test><style>text</style></test>"
+    end
     it "leaves the tag unopened if doesn't encounter a pair of keys" do
       subject.content = "$ttext"
       subject.tags['t'] = ["<tag>", "</tag>"]
@@ -49,12 +61,12 @@ describe Formattable do
   it "complains if the class doesn't have a content" do
     including_on_contentless_class = lambda {class ContentlessClass; include Formattable; end}
     including_on_contentless_class.should raise_error
-  end
+end
 
-  describe "#paragraphs" do
-    it "returns an array containing the paragraphs on the content (stripped)" do
-      subject.content = "Paragraph 1\n\n   Paragraph 2\n\n\nParagraph 3\nLine 1  "
-      subject.paragraphs.should == ["Paragraph 1", "Paragraph 2", "Paragraph 3\nLine 1"]
-    end
+describe "#paragraphs" do
+  it "returns an array containing the paragraphs on the content (stripped)" do
+    subject.content = "Paragraph 1\n\n   Paragraph 2\n\n\nParagraph 3\nLine 1  "
+    subject.paragraphs.should == ["Paragraph 1", "Paragraph 2", "Paragraph 3\nLine 1"]
   end
+end
 end
