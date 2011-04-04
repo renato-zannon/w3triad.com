17ca6295016782d29d7a286c1928ab875d3150c6
Renato Riccieri <renato.riccieri@gmail.com>
2011-04-04 13:12:40 -0300

    Created simple 'latest posts' section

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -37,6 +37,6 @@ Then /^I should be able to see my info$/ do
   Then "I should see /Nickname.*Bill/"
   Then "I should see /Name.*Renato/"
   Then "I should see /Bio.*/"
-  Then "I should see /Email.*renato\.riccieri@gmail.com/"
-  Then "I should see /Last posts/"
+  Then "I should see /E-mail.*renato\.riccieri@gmail.com/"
+  Then "I should see /Latest posts/"
 end
--- features/users_profile.feature
+++ features/users_profile.feature
@@ -1,6 +1,5 @@
 Feature: User profiles
 
-    @wip
     Scenario: View user profile
       Given I am an existent User
       When I go to my profile's page
