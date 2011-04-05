a8c0219d9b435679b6e698611abad06db7b44b60
Renato Riccieri <renato.riccieri@gmail.com>
2011-04-05 15:07:37 -0300

    Created a mechanism to update a user's password, along with a validations refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessor :password
+  attr_accessor :password, :new_password
   attr_protected :password_salt, :password_hash
 
   has_many :posts, :foreign_key => :author_id
 
-  validates   :password, :confirmation => true, :presence => true
-  validates   :email,    :email        => true, :presence => true
-  validates   :name,     :presence     => true
+  validates_presence_of :nickname, :email, :name, :password
+  validates_confirmation_of :password,              :on => :create
+  validates_presence_of :password_confirmation,     :on => :create
+  validates_confirmation_of :new_password
+  validates_presence_of :new_password_confirmation, :on => :update, :unless => Proc.new { new_password.nil? or new_password.empty? }
+  validates :email, :email => true
 
-  before_save :encrypt_password
+  before_save   :encrypt_password
+  validate(:check_password, :on => :update)
 
   def has_valid_password?
-    password_hash == encrypt(password)
+    self.password_hash == encrypt(self.password)
   end
 
   def User.authenticate(email, password)
@@ -19,10 +23,11 @@ class User < ActiveRecord::Base
     user.password = password unless user.nil?
     return user if user && user.has_valid_password?
   end
-  private
 
+  private
   def encrypt_password
-    self.password_hash = encrypt(password)
+    self.password = self.new_password unless self.new_password.nil? or self.new_password.empty?
+    self.password_hash = encrypt(self.password)
   end
 
   def encrypt(pass)
@@ -30,4 +35,7 @@ class User < ActiveRecord::Base
     BCrypt::Engine.hash_secret(pass, password_salt)
   end
 
+  def check_password
+    self.errors[:password] << "not valid!" unless has_valid_password?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper.rb'
 describe User do
 
   let(:user) { Factory(:user) }
+  let(:new_user) { Factory.build(:user) }
 
   describe "validations" do
     it "is valid with valid attributes" do
@@ -14,9 +15,9 @@ describe User do
       user.should_not be_valid
     end
 
-    it "is invalid without a password confirmation" do
-      user.password_confirmation = ""
-      user.should_not be_valid
+    it "a new user is invalid without a password confirmation" do
+      new_user.password_confirmation = ""
+      new_user.should_not be_valid
     end
 
     it "is invalid with incompatible password and password_confirmation" do
@@ -58,12 +59,12 @@ describe User do
 
   describe ".authenticate" do
     it "returns the user, if a valid combination of password and email is provided" do
-      user.password = 'password'
-      user.password_confirmation = 'password'
-      user.email = 'email@example.com'
-      user.save
+      new_user.password = 'password'
+      new_user.password_confirmation = 'password'
+      new_user.email = 'email@example.com'
+      new_user.save
 
-      User.authenticate(user.email, user.password).should == user
+      User.authenticate(new_user.email, new_user.password).should == new_user
     end
 
     it "returns nil if a nonexistant email is passed" do
@@ -76,6 +77,41 @@ describe User do
 
     it "returns nil if given both a wrong password and a nonexistant email" do
       User.authenticate("doesntexist@nowhere.com", "wrongpassword").should be_nil
+    end
+  end
+
+  describe "setting a new password" do
+    let(:user) { Factory.create :user, :password              => "password",
+                                       :password_confirmation => "password" }
+    it "works if the current password is set correctly" do
+      user.password = "password"
+      user.new_password = "newpassword"
+      user.new_password_confirmation = "newpassword"
+      user.should be_valid
+    end
+
+    it "doesn't work if the current password is incorrect" do
+      user.password = "wrongpassword"
+      user.new_password = "newpassword"
+      user.new_password_confirmation = "newpassword"
+      user.should_not be_valid
+    end
+
+    it "doesn't work if a password confirmation isn't supplied or is wrong" do
+      user.password = "password"
+      user.new_password = "newpassword"
+      user.should_not be_valid
+
+      user.new_password_confirmation = "wrongnewpassword"
+      user.should_not be_valid
+    end
+
+    it "does nothing if new_password isn't supplied" do
+      user.password = "password"
+      user.should be_valid
+
+      user.save
+      user.password.should == "password"
     end
   end
 end
