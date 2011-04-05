f9debd791f4ae45752f91d14c53c2a66b664d95e
Renato Riccieri <renato.riccieri@gmail.com>
2011-04-05 15:39:36 -0300

    Created user's profile-editing form

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ W3triadCom::Application.routes.draw do
   get "users/show"
 
   root :to => "posts#index"
-  match '/posts/preview'     => "posts#preview",    :as => "preview_post"
+  match '/posts/preview'     => "posts#preview",      :as => "preview_post"
   resources :posts
 
-  match '/admin'             => "sessions#new",     :as => "login"
-  match '/admin/logout'      => "sessions#destroy", :as => "logout"
+  match '/admin'             => "sessions#new",       :as => "login"
+  match '/admin/logout'      => "sessions#destroy",   :as => "logout"
   resources :sessions
 
-  match '/:nickname'         => "users#show",       :as => "profile"
+  match '/:nickname'         => "users#show",         :as => "profile",      :method => :get
+  match '/admin/:id/edit'    => "users#edit_profile", :as => "edit_profile", :method => :get
+  match '/admin/:id'         => "users#update",       :as => "update_user",  :method => :put
 end
--- features/users_profile.feature
+++ features/users_profile.feature
@@ -4,3 +4,10 @@ Feature: User profiles
       Given I am an existent User
       When I go to my profile's page
       Then I should be able to see my info
+
+    Scenario: View "edit profile" page
+      Given I am an existent User
+      And I am logged in
+      When I go to my profile's page
+      And I press "Edit Profile"
+      Then I should see a form to edit my profile
