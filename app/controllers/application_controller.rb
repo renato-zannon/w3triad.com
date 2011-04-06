096086f9016c51859d1fd4c3f8f7022a52f04651
Renato Riccieri <renato.riccieri@gmail.com>
2011-04-06 16:47:13 -0300

    Redirect everything to the international domain, shall we?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :redirect_to_international
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) unless session[:user_id].nil? rescue nil
+  end
+
+  def redirect_to_international
+    return unless request.host.match /\.br/ and Rails.env == "production"
+    url = request.protocol + request.host_with_port.sub(/^(www\.)?w3triad\.com\.br/, '\1w3triad.com') + request.fullpath
+    lang_opt = request.request_uri.match(/\?/) ? "&lang=pt" : "?lang=pt"
+    redirect_to url + lang_opt, :status => :moved_permanently
   end
 end
