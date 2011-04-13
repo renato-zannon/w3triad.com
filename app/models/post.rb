afdc2e60b50a241aaff16cd7823b1067f9a0de8a
Renato Riccieri <renato.riccieri@gmail.com>
2011-04-13 11:23:20 -0300

    Added a few tags to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,5 +16,7 @@ class Post < ActiveRecord::Base
                        :paragraph_begin => '<p>',
                        :paragraph_end   => '</p>',
                        :line_break      => '<br />',
-                       '(span)'             => lambda { |style| ["<span class='#{style}'>", "</span>"] }
+                       '(span)'         => lambda { |style| ["<span class='#{style}'>", "</span>"] },
+                       '(img)'          => lambda { |url|   ["<img src='#{url}'>", ""] },
+                       '(link)'         => lambda { |url|   ["<a href='#{url}'>", "</a>"]}
 end
